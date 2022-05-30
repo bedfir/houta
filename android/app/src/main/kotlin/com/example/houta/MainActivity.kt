d4e7333fb70dd0a2710d7250de72398873efc1c2
Bedoui <firas-bedoui@hotmail.fr>
2022-05-30 20:16:40 +0200

    houta

---
+++ android/app/src/main/kotlin/com/example/houta/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.houta
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,66 @@
+import 'dart:io';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:houta/common/theme.dart';
+import 'package:houta/models/cart.dart';
+import 'package:houta/models/catalog.dart';
+import 'package:houta/views/cart.dart';
+import 'package:houta/views/catalog.dart';
+import 'package:houta/views/login.dart';
+import 'package:provider/provider.dart';
+import 'package:window_size/window_size.dart';
+
+void main() {
+  setupWindow();
+  runApp(const MyApp());
+}
+
+const double windowWidth = 400;
+const double windowHeight = 800;
+
+void setupWindow() {
+  if (!kIsWeb && (Platform.isWindows || Platform.isLinux || Platform.isMacOS)) {
+    WidgetsFlutterBinding.ensureInitialized();
+    setWindowTitle('Provider Demo');
+    setWindowMinSize(const Size(windowWidth, windowHeight));
+    setWindowMaxSize(const Size(windowWidth, windowHeight));
+    getCurrentScreen().then((screen) {
+      setWindowFrame(Rect.fromCenter(
+        center: screen!.frame.center,
+        width: windowWidth,
+        height: windowHeight,
+      ));
+    });
+  }
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        Provider(create: (context) => CatalogModel()),
+        ChangeNotifierProxyProvider<CatalogModel, CartModel>(
+          create: (context) => CartModel(),
+          update: (context, catalog, cart) {
+            if (cart == null) throw ArgumentError.notNull('cart');
+            cart.catalog = catalog;
+            return cart;
+          },
+        ),
+      ],
+      child: MaterialApp(
+        title: 'Provider Demo',
+        theme: appTheme,
+        initialRoute: '/',
+        routes: {
+          '/': (context) => const MyLogin(),
+          '/catalog': (context) => const MyCatalog(),
+          '/cart': (context) => const MyCart(),
+        },
+      ),
+    );
+  }
+}
